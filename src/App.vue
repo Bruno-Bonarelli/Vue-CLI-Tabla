9e541934c293de0a6603ce0b4b74da0cbe676111
bona <brunobonarelli@gmail.com>
2022-05-14 13:02:26 -0300

    V 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Registro de personas</h2>
+    <TablaClaseCuatro :titulos="tabla1.titulos" :registros="tabla1.registros" >
+    </TablaClaseCuatro>
+    <h2>Productos</h2>
+    <TablaClaseCuatro :titulos="tabla2.titulos" :registros="tabla2.registros">
+    </TablaClaseCuatro>
+    <h2>Ranking ATP</h2>
+    <TablaClaseCuatro :titulos="tabla3.titulos" :registros="tabla3.registros">
+    </TablaClaseCuatro>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaClaseCuatro from './components/TablaClaseCuatro.vue'; 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TablaClaseCuatro
+  },
+  data(){
+        return{
+            tabla1: {
+            titulos: ["DNI","Nombre", "Apellido", "Estado civil"],
+            registros: [
+                ["35.459.554","Jeremias", "Orcajo", "Casado"],
+                ["29.659.996","Juliana", "Etchanchu", "Soltero"],
+                ["21.344.567","Marisol", "Sanchez", "Casado"],
+                ["21.545.284","Eugenia", "Melazco", "Casado"],
+            ]
+        },
+        tabla2: {
+            titulos: ["Producto", "Disponible", "Valor"],
+            registros: [
+                ["Leche", "Si", "$150"],
+                ["Yerba", "No", "$250"],
+                ["Arroz", "Si", "$170"],
+                ["Pan", "Si", "$40"],
+            ]
+        },
+        tabla3: {
+            titulos: ["Posicion", "Tenista", "Puntos"],
+            registros: [
+                ["1°", "Novak Djokovic", "8400"],
+                ["2°", "Daniil Medvedev", "8080"],
+                ["3°", "Alexander Zverev", "7420"],
+                ["4°", "Rafael Nadal", "6435"],
+            ]
+        },
+        }
   }
+  
 }
 </script>
 
---
+++ src/components/TablaClaseCuatro.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <table class="table table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(registro, i) in registros" :key="i">
+                        
+                    <th v-for="(row, r) in registro" :key="r">{{row}}</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TablaClaseCuatro',
+    props: {
+        titulos: {
+            type: Array,
+            required: true
+        },
+        registros: {
+            type: Array,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
